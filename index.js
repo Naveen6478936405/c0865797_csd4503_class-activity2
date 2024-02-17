d33ae55388c2b0b8e11bda6aded9d5fc9ae31c27
Naveen6478936405 <157910255+Naveen6478936405@users.noreply.github.com>
2024-02-17 13:59:36 -0500

    Naveen baabu

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const SERVER_PORT = process.env.PORT || 3000;
 app.use(express.static("./views"));
 
 // http://localhost:3000/
-app.get('/C0867367', function (req, res) {
-  res.send('<h1>Hello - C0867367 - Nishta</h1> ')
+app.get('/C0865797', function (req, res) {
+  res.send('<h1>Hello - C0865797 - Naveen</h1> ')
 })
 
 // http://localhost:3000/hello
